4d4e1e6814074202404adffff45405ea74304155
pflink <pontus.flink@gmail.com>
2016-04-06 13:33:13 +0300

    Fix user agent 403 error; make exceptions not swallow keyboardinterrupt exceptions; add first_name to get_user_info

--- happn/happn.py
+++ happn/happn.py
@@ -19,73 +19,75 @@ CLIENT_SECRET = config('CLIENT_SECRET')
 # Default headers for making Happn API calls
 headers = {
     'http.useragent':'Happn/1.0 AndroidSDK/0',
-    'User-Agent':'Dalvik/1.6.0 (Linux; U; Android 4.4.2; SCH-I535 Build/KOT49H)',
+    'User-Agent':'Happn/19.1.0 AndroidSDK/19',
+    'platform': 'android',
     'Host':'api.happn.fr',
     'connection' : 'Keep-Alive',
     'Accept-Encoding':'gzip'
 }
 
 httpErrors = {
-	200 : 'OK',
-	201 : 'Created',
-	202 : 'Accepted',
-	203 : 'Non-authoritative Information',
-	204 : 'No Content',
-	205 : 'Reset Content',
-	206 : 'Partial Content',
-	207 : 'Multi-Status',
-	208 : 'Already Reported',
-	226 : 'IM Used',
-	300 : 'Multiple Choices',
-	301 : 'Moved Permanently',
-	302 : 'Found',
-	303 : 'See Other',
-	304 : 'Not Modified',
-	305 : 'Use Proxy',
-	307 : 'Temporary Redirect',
-	308 : 'Permanent Redirect',
-	400 : 'Bad Request',
-	401 : 'Unauthorized',
-	402 : 'Payment Required',
-	403 : 'Forbidden',
-	404 : 'Not Found',
-	405 : 'Method Not Allowed',
-	406 : 'Not Acceptable',
-	407 : 'Proxy Authentication Required',
-	408 : 'Request Timeout',
-	409 : 'Conflict',
-	410 : 'Gone',
-	411 : 'Length Required',
-	412 : 'Precondition Failed',
-	413 : 'Payload Too Large',
-	414 : 'Request-URI Too Long',
-	415 : 'Unsupported Media Type',
-	416 : 'Requested Range Not Satisfiable',
-	417 : 'Expectation Failed',
-	418 : 'I\'m a teapot',
-	421 : 'Misdirected Request',
-	422 : 'Unprocessable Entity',
-	423 : 'Locked',
-	424 : 'Failed Dependency',
-	426 : 'Upgrade Required',
-	428 : 'Precondition Required',
-	429 : 'Too Many Requests',
-	431 : 'Request Header Fields Too Large',
-	444 : 'No Response (Nginx)',
-	451 : 'Unavailable For Legal Reasons',
-	499 : 'Client Closed Request',
-	500 : 'Internal Server Error',
-	501 : 'Not Implemented',
-	502 : 'Bad Gateway',
-	503 : 'Service Unavailable',
-	504 : 'Gateway Timeout',
-	505 : 'HTTP Version Not Supported',
-	506 : 'Variant Also Negotiates',
-	507 : 'Insufficient Storage',
-	508 : 'Loop Detected',
-	510 : 'Not Extended',
-	511 : 'Network Authentication Required',
-	599 : 'Network Connect Timeout Error',
+    200 : 'OK',
+    201 : 'Created',
+    202 : 'Accepted',
+    203 : 'Non-authoritative Information',
+    204 : 'No Content',
+    205 : 'Reset Content',
+    206 : 'Partial Content',
+    207 : 'Multi-Status',
+    208 : 'Already Reported',
+    226 : 'IM Used',
+    300 : 'Multiple Choices',
+    301 : 'Moved Permanently',
+    302 : 'Found',
+    303 : 'See Other',
+    304 : 'Not Modified',
+    305 : 'Use Proxy',
+    307 : 'Temporary Redirect',
+    308 : 'Permanent Redirect',
+    400 : 'Bad Request',
+    401 : 'Unauthorized',
+    402 : 'Payment Required',
+    403 : 'Forbidden',
+    404 : 'Not Found',
+    405 : 'Method Not Allowed',
+    406 : 'Not Acceptable',
+    407 : 'Proxy Authentication Required',
+    408 : 'Request Timeout',
+    409 : 'Conflict',
+    410 : 'Gone',
+    411 : 'Length Required',
+    412 : 'Precondition Failed',
+    413 : 'Payload Too Large',
+    414 : 'Request-URI Too Long',
+    415 : 'Unsupported Media Type',
+    416 : 'Requested Range Not Satisfiable',
+    417 : 'Expectation Failed',
+    418 : 'I\'m a teapot',
+    421 : 'Misdirected Request',
+    422 : 'Unprocessable Entity',
+    423 : 'Locked',
+    424 : 'Failed Dependency',
+    426 : 'Upgrade Required',
+    428 : 'Precondition Required',
+    429 : 'Too Many Requests',
+    431 : 'Request Header Fields Too Large',
+    444 : 'No Response (Nginx)',
+    451 : 'Unavailable For Legal Reasons',
+    499 : 'Client Closed Request',
+    500 : 'Internal Server Error',
+    501 : 'Not Implemented',
+    502 : 'Bad Gateway',
+    503 : 'Service Unavailable',
+    504 : 'Gateway Timeout',
+    505 : 'HTTP Version Not Supported',
+    506 : 'Variant Also Negotiates',
+    507 : 'Insufficient Storage',
+    508 : 'Loop Detected',
+    510 : 'Not Extended',
+    511 : 'Network Authentication Required',
+    522 : 'Connection timed out, server denied request for OAuth token',
+    599 : 'Network Connect Timeout Error',
     #@TODO Add full list
 }
 
@@ -112,7 +114,7 @@ class User:
         else:
             self.set_position(latitude, longitude)
 
-        logging.info('Happn User Generated. ID: %s', self.id)
+        logging.debug('Happn User Generated. ID: %s', self.id)
 
     def set_position(self, latitude, longitude):
         """ Set the position of the user using Happn's API
@@ -140,7 +142,7 @@ class User:
         if r.status_code == 200:    #OK HTTP status
             self.lat = latitude
             self.lon = longitude
-            logging.info('Set User position at %f, %f', self.lat, self.lon)
+            logging.debug('Set User position at %f, %f', self.lat, self.lon)
         else:
             # Status failed, get the current location according to the server
             #@TODO IMPLEMENT ^
@@ -181,11 +183,11 @@ class User:
         url = 'https://api.happn.fr/api/users/' + self.id + '/devices/'+ config('DEVICE_ID')
         try:
             r = requests.put(url,headers=h,data=json.dumps(payload))
-        except:
-            raise HTTP_MethodError('Error Connecting to Happn Server')
+        except Exception as e:
+            raise HTTP_MethodError('Error Setting Device: {}'.format(e))
 
         if r.status_code == 200: #200 = 'OK'
-            logging.info('Device Set')
+            logging.debug('Device Set')
         else:
             # Device set denied by server
             logging.warning('Server denied request for device set change: %d', r.status_code)
@@ -202,11 +204,11 @@ class User:
         url = 'https://api.happn.fr/api/users/' + self.id
         try:
             r = requests.put(url, headers=h, data = json.dumps(settings))
-        except:
-            raise HTTP_MethodError('Error Connecting to Happn Server')
+        except Exception as e:
+            raise HTTP_MethodError('Error Setting Settings: {}'.format(e))
 
         if r.status_code == 200: #200 = 'OK'
-            logging.info('Updated Settings')
+            logging.debug('Updated Settings')
         else:
             # Unable to fetch distance
             raise HTTP_MethodError(httpErrors[r.status_code])
@@ -229,8 +231,8 @@ class User:
 
         try:
             r = requests.get(url, headers=h)
-        except:
-            raise HTTP_MethodError('Error Connecting to Happn Server')
+        except Exception as e:
+            raise HTTP_MethodError('Error Connecting to Happn Server: {}'.format(e))
 
         if r.status_code == 200: #200 = 'OK'
             self.distance = r.json()['data']['distance']
@@ -246,7 +248,7 @@ class User:
         h=headers
         h.update({
             'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
-            'Content-Length': '439'
+            'Content-Length': '374'
             })
 
         payload = {
@@ -260,12 +262,12 @@ class User:
         url = 'https://api.happn.fr/connect/oauth/token'
         try:
             r = requests.post(url,headers=h,data=payload)
-        except:
-            raise HTTP_MethodError('Error Connecting to Happn Server')
+        except Exception as e:
+            raise HTTP_MethodError('Error Connecting to Happn Server: {}'.format(e))
 
         # Check response validity
         if r.status_code == 200: #200 = 'OK'
-            logging.info('Fetched Happn OAuth token:, %s', r.json()['access_token'])
+            logging.debug('Fetched Happn OAuth token:, %s', r.json()['access_token'])
             return r.json()['access_token'], r.json()['user_id']
         else:
             # Error code returned from server (but server was accessible)
@@ -286,18 +288,18 @@ class User:
             'http.useragent' : 'Happn/1.0 AndroidSDK/0',
             'Authorization'  : 'OAuth="' + self.oauth+'"',
             'Content-Type'   : 'application/json',
-            'User-Agent'     : 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; SCH-I535 Build/KOT49H)',
+            'User-Agent'     : 'Happn/19.1.0 AndroidSDK/19',
             'Host'           : 'api.happn.fr',
             'Connection'     : 'Keep-Alive',
             'Accept-Encoding': 'gzip'
         }
 
-        query = '?query=%7B%22fields%22%3A%22about%2Cis_accepted%2Cage%2Cjob%2Cworkplace%2Cmodification_date%2Cprofiles.mode%281%29.width%28720%29.height%281280%29.fields%28url%2Cwidth%2Cheight%2Cmode%29%2Clast_meet_position%2Cmy_relation%2Cis_charmed%2Cdistance%2Cgender%2Cmy_conversation%22%7D'
+        query = '?query=%7B%22fields%22%3A%22about%2Cis_accepted%2Cfirst_name%2Cage%2Cjob%2Cworkplace%2Cmodification_date%2Cprofiles.mode%281%29.width%28720%29.height%281280%29.fields%28url%2Cwidth%2Cheight%2Cmode%29%2Clast_meet_position%2Cmy_relation%2Cis_charmed%2Cdistance%2Cgender%2Cmy_conversation%22%7D'
         url = 'https://api.happn.fr/api/users/' + userID + query
         try:
             r = requests.get(url, headers=h)
-        except:
-            raise HTTP_MethodError('Error Connecting to Happn Server')
+        except Exception as e:
+            raise HTTP_MethodError('Error Connecting to Happn Server: {}'.format(e))
 
         # Check if successful
         if r.status_code == 200: #200 = 'OK'
@@ -305,20 +307,20 @@ class User:
             return json.loads(json.dumps(r.json()['data'], sort_keys=True, indent=4, separators=(',', ': ')))
         else:
             raise HTTP_MethodError(httpErrors[r.status_code])
-
-
+    
+            
     def get_recommendations(self, limit=16, offset=0):
         """ Get recs from Happn server
             :param limit Number of reccomendations to recieve
             :param offset Offset index for reccomendation list
         """
-
+        
         # Create and send HTTP Get to Happn server
         h={ #For some reason header update doesnt work
             'http.useragent' : 'Happn/1.0 AndroidSDK/0',
             'Authorization'  : 'OAuth="' + self.oauth+'"',
             'Content-Type'   : 'application/json',
-            'User-Agent'     : 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; SCH-I535 Build/KOT49H)',
+            'User-Agent'     : 'Happn/19.1.0 AndroidSDK/19',
             'Host'           : 'api.happn.fr',
             'Connection'     : 'Keep-Alive',
             'Accept-Encoding': 'gzip'
@@ -328,15 +330,14 @@ class User:
 
         try:
             r = requests.get(url, headers=h)
-        except:
-            raise HTTP_MethodError('Error Connecting to Happn Server')
-
+        except Exception as e:
+            raise HTTP_MethodError('Error Connecting to Happn Server: {}'.format(e))
+            
         if r.status_code == 200: #200 = 'OK'
             return json.loads(json.dumps(r.json()['data'], sort_keys=True, indent=4, separators=(',', ': ')))
         else:
             raise HTTP_MethodError(httpErrors[r.status_code])
-
-
+    
     def update_activity(self):
         """ Updates User activity """
 
@@ -353,11 +354,11 @@ class User:
         url = 'https://api.happn.fr/api/users/'+self.id
         try:
             r = requests.put(url, headers=h, data = payload)
-        except:
-            raise HTTP_MethodError('Error Connecting to Happn Server')
+        except Exception as e:
+            raise HTTP_MethodError('Error Connecting to Happn Server: {}'.format(e))
 
         if r.status_code == 200: #200 = 'OK'
-            logging.info('Updated User activity')
+            logging.debug('Updated User activity')
         else:
             # Unable to fetch distance
             raise HTTP_MethodError(httpErrors[r.status_code])
@@ -380,15 +381,15 @@ class User:
         url = 'https://api.happn.fr/api/users/' + self.id +'/accepted/'+str(user_id)
         try:
             r = requests.post(url, headers=h, data = payload)
-        except:
-            raise HTTP_MethodError('Error Connecting to Happn Server')
+        except Exception as e:
+            raise HTTP_MethodError('Error Connecting to Happn Server: {}'.format(e))
 
         if r.status_code == 200: #200 = 'OK'
-            logging.info('Liked User '+str(user_id))
+            logging.debug('Liked User '+str(user_id))
         else:
             # Unable to fetch distance
             raise HTTP_MethodError(httpErrors[r.status_code])
-
+    
     def decline_user(self, user_id):
         """ Decline user
             :user_id id of the user to decline
@@ -407,15 +408,15 @@ class User:
         url = 'https://api.happn.fr/api/users/' + self.id +'/rejected/'+str(user_id)
         try:
             r = requests.post(url, headers=h, data = payload)
-        except:
-            raise HTTP_MethodError('Error Connecting to Happn Server')
+        except Exception as e:
+            raise HTTP_MethodError('Error Connecting to Happn Server: {}'.format(e))
 
         if r.status_code == 200: #200 = 'OK'
             logging.debug('Declined User '+str(user_id))
         else:
             # Unable to fetch distance
-            raise HTTP_MethodError(httpErrors[r.status_code])           
-            
+            raise HTTP_MethodError(httpErrors[r.status_code])
+
 
 class HTTP_MethodError(Exception):
     def __init__(self, value):
